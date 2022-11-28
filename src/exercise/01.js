769af14eb76794b147c664df1374652504c2cc1b
mi5ha6in <mi5ha6in@gmail.com>
2022-11-28 23:59:48 +0300

    refactoring task 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -76,12 +76,16 @@ function useUser() {
 // 🐨 add a function here called `updateUser`
 // Then go down to the `handleSubmit` from `UserSettings` and put that logic in
 // this function. It should accept: dispatch, user, and updates
-function updatedUser(dispatch, user, updates) {
+async function updatedUser(dispatch, user, updates) {
   dispatch({type: 'start update', updates})
-  userClient.updateUser(user, updates).then(
-    updatedUser => dispatch({type: 'finish update', updatedUser}),
-    error => dispatch({type: 'fail update', error}),
-  )
+  try {
+    const updatedUser = await userClient.updateUser(user, updates)
+    dispatch({type: 'finish update', updatedUser})
+    return updatedUser
+  } catch (error) {
+    dispatch({type: 'fail update', error})
+    return Promise.reject(error)
+  }
 }
 // export {UserProvider, useUser}
 
